39940fd928bb4d1deef1aef58f47563a52a8b657
omkarpingale143 <150931691+omkarpingale143@users.noreply.github.com>
2023-11-16 10:22:32 +0530

    bitwise.java

---
+++ bitwise.java
@@ -0,0 +1,17 @@
+public class bitwise 
+{
+ public static void main(String[] args) 
+  {
+  //Variables Decleration
+  int num1 = 30, num2 = 6, num3 =0;
+
+  //Bitwise AND
+  System.out.println("num1 & num2 = " + (num1 & num2));
+  
+  //Bitwise OR
+  System.out.println("num1 | num2 = " + (num1 | num2) );
+  
+  //Bitwise XOR
+  System.out.println("num1 ^ num2 = " + (num1 ^ num2) );
+ }
+}
