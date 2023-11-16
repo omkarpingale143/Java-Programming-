145312d699b07ff756533101f7102226e9a52c08
omkarpingale143 <150931691+omkarpingale143@users.noreply.github.com>
2023-11-16 10:57:19 +0530

    circle.java

---
+++ circle.java
@@ -0,0 +1,19 @@
+// Java program to calculate the area of the
+public class circle
+{
+    public static void main(String[] args)
+    {
+        int radius;
+        double pi = 3.142, area;
+        radius = 5;
+      
+        // area of the circle
+        area = pi * radius * radius;
+        System.out.println("Area of circle is :" + area);
+      
+        //circumference of the circle
+        circumference= pi*2*radius;
+        System.out.println("The circumference of the circle is: "+circumference);
+    }
+}
+
